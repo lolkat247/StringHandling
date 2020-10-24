ca9f0d7565f55cb5a0ff19dc53a5be19f0eddb04
Jacob Iglesias <jacob.ds.iglesias.5@gmail.com>
2020-10-23 22:57:36 -0400

    Add project files.

---
+++ StringHandling/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace StringHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.tbState = new System.Windows.Forms.TextBox();
+            this.tbZipCode = new System.Windows.Forms.TextBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "City:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "State:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Zip Code:";
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(71, 48);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(172, 20);
+            this.tbCity.TabIndex = 2;
+            // 
+            // tbState
+            // 
+            this.tbState.Location = new System.Drawing.Point(71, 70);
+            this.tbState.Name = "tbState";
+            this.tbState.Size = new System.Drawing.Size(34, 20);
+            this.tbState.TabIndex = 3;
+            // 
+            // tbZipCode
+            // 
+            this.tbZipCode.Location = new System.Drawing.Point(71, 93);
+            this.tbZipCode.Name = "tbZipCode";
+            this.tbZipCode.Size = new System.Drawing.Size(100, 20);
+            this.tbZipCode.TabIndex = 4;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(249, 4);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 1;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(249, 90);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat.TabIndex = 5;
+            this.btnFormat.Text = "Format";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(249, 132);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Email:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(71, 6);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(172, 20);
+            this.tbEmail.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(328, 161);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.tbZipCode);
+            this.Controls.Add(this.tbState);
+            this.Controls.Add(this.tbCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "String Handling";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.TextBox tbState;
+        private System.Windows.Forms.TextBox tbZipCode;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbEmail;
+    }
+}
+
---
+++ StringHandling/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            // vars
+            string email, username, domain;
+
+            // var in
+            try
+            {
+                email = tbEmail.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+                return;
+            }
+
+            // validate data
+            if (!email.Contains("@"))
+            {
+                MessageBox.Show("Non-email input", "Format Error");
+                return;
+            }
+
+            // format data
+            username = email.Split('@')[0];
+            domain = email.Split('@')[1];
+
+            // display data
+            MessageBox.Show(
+                // body
+                "User name: " + username + '\n' +
+                "Domain name: " + domain,
+
+                // title
+                "Parsed String"
+                );
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            // vars 
+            string city, state, zip;
+            
+            // get data
+            try
+            {
+                city = tbCity.Text;
+                state = tbState.Text;
+                zip = tbZipCode.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+                return;
+            }
+
+            // NO validation required
+
+            // format data
+            state = state.ToUpper();
+
+            // display data
+            MessageBox.Show(
+                // body
+                "City, State, Zip: " + city + ", " + state + ' ' + zip,
+
+                // title
+                "Formatted String"
+                );
+
+        }
+    }
+}
